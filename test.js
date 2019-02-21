3769dad07e39cce1d6006ee359ccad4cd0490f57
Ignacio Cruz <igmoweb@gmail.com>
2019-02-21 15:58:33 +0100

    Delete old file

--- test.js
+++
@@ -1,3 +0,0 @@
-const fileExists = require( './utils/fileExists' );
-const result = fileExists('/Users/ignacio/source/vvv/www/wordpress-multisite/public_html/wp-content/themes/wp-theme/src/js/index.js');
-console.log( result );
